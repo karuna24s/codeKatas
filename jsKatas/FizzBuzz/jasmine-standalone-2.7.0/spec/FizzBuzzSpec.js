79a56030f15b2ccb5a66ddb4e68cc94fdf6a69de
karuna24s <karuna24s@gmail.com>
2017-08-05 22:49:12 -0400

    update FizzBuzzSpec file

--- jsKatas/FizzBuzz/jasmine-standalone-2.7.0/spec/FizzBuzzSpec.js
+++ jsKatas/FizzBuzz/jasmine-standalone-2.7.0/spec/FizzBuzzSpec.js
@@ -1,23 +1,23 @@
 describe('FizzBuzz', function () {
-  it('should return 1 when 1 is passed', function () {
+  it('returns 1 when 1 is passed', function () {
     expect(FizzBuzz(1)).toEqual(1)
   })
-  it('should return 2 when 2 is passed', function () {
+  it('returns 2 when 2 is passed', function () {
     expect(FizzBuzz(2)).toEqual(2)
   })
-  it('should return Fizz when 3 is passed', function () {
+  it('returns Fizz when 3 is passed', function () {
     expect(FizzBuzz(3)).toEqual('Fizz')
   })
-  it('should return Buzz when 5 is passed', function () {
+  it('returns Buzz when 5 is passed', function () {
     expect(FizzBuzz(5)).toEqual('Buzz')
   })
-  it('should return Fizz when 6 is passed', function () {
+  it('returns Fizz when 6 is passed', function () {
     expect(FizzBuzz(6)).toEqual('Fizz')
   })
-  it('should return Buzz when 10 is passed', function () {
+  it('returns Buzz when 10 is passed', function () {
     expect(FizzBuzz(10)).toEqual('Buzz')
   })
-  it('should return FizzBuzz when 15 is passed', function () {
+  it('returns FizzBuzz when 15 is passed', function () {
     expect(FizzBuzz(15)).toEqual('FizzBuzz')
   })
 })
