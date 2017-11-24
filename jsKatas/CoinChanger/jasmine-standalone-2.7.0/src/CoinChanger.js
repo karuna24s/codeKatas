62cd7fb7723def28b35b3694d725bddc6b064da0
karuna24s <karuna24s@gmail.com>
2017-11-24 18:38:56 -0500

    add CoinChanger codekata to jsKatas folder

---
+++ jsKatas/CoinChanger/jasmine-standalone-2.7.0/src/CoinChanger.js
@@ -0,0 +1,10 @@
+function CoinChanger(amount) {
+  var change = [], total = 0;
+  [25, 10, 5, 1].forEach(function(coin) {
+    while (total + coin <= amount) {
+      change.push(coin);
+      total += coin;
+    }
+  });
+  return change;
+};
