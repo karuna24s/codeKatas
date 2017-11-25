b839a52d7a41894de40453155b62e7281e8ffb8e
karuna24s <karuna24s@gmail.com>
2017-11-25 13:14:42 -0500

    add second version of coin changers js code kata

--- jsKatas/CoinChanger/jasmine-standalone-2.7.0/spec/CoinChangerSpec.js
+++ jsKatas/CoinChanger/jasmine-standalone-2.7.0/spec/CoinChangerSpec.js
@@ -1,41 +1,26 @@
 describe('CoinChanger', function () {
-  it ('returns 1 penny for 1 penny', function () {
+  it ('returns 1 penny for 1', function () {
     expect(CoinChanger(1)).toEqual([1])
   })
-
-  it ('returns 3 pennies for 3', function () {
-    expect(CoinChanger(3)).toEqual([1, 1, 1])
+  it ('returns 4 pennies for 4', function () {
+    expect(CoinChanger(4)).toEqual([1, 1, 1, 1])
   })
-
-  it ('returns 1 nickel for 5', function () {
+  it ('returns 1 nickel for', function () {
     expect(CoinChanger(5)).toEqual([5])
   })
-
-  it ('returns 1 dime for 10', function () {
+  it ('returns 1 nickel, 4 pennies for 9', function () {
+    expect(CoinChanger(9)).toEqual([5, 1, 1, 1, 1])
+  })
+  it ('returns 1 dime for 1', function () {
     expect(CoinChanger(10)).toEqual([10])
   })
-
-  it ('returns 2 dimes for 20', function () {
-    expect(CoinChanger(20)).toEqual([10, 10])
+  it ('returns 1 dime, 1 nickel for 15', function () {
+    expect(CoinChanger(15)).toEqual([10, 5])
   })
-
   it ('returns 1 quarter for 25', function () {
     expect(CoinChanger(25)).toEqual([25])
   })
-
-  it ('returns 2 quarters for 50', function () {
-    expect(CoinChanger(50)).toEqual([25, 25])
-  })
-
-  it ('returns 3 quarters for 75', function () {
-    expect(CoinChanger(75)).toEqual([25, 25, 25])
-  })
-
-  it ('returns 2 dimes and 4 pennies for 24', function () {
-    expect(CoinChanger(24)).toEqual([10, 10, 1, 1, 1, 1])
-  })
-
-  it ('returns 3 quarters, 2 dimes, and 4 pennies for 99', function () {
-    expect(CoinChanger(99)).toEqual([25, 25, 25, 25, 10, 10, 1, 1, 1, 1])
+  it ('returns 3 quarters, 1 dime, 5, 1 nickel, 4 pennies for 25', function () {
+    expect(CoinChanger(94)).toEqual([25, 25, 25, 10, 5, 1, 1, 1, 1])
   })
 })
--- jsKatas/CoinChanger/jasmine-standalone-2.7.0/src/CoinChanger.js
+++ jsKatas/CoinChanger/jasmine-standalone-2.7.0/src/CoinChanger.js
@@ -1,5 +1,5 @@
 function CoinChanger(amount) {
-  var change = [], total = 0;
+  let change = [], total = 0;
   [25, 10, 5, 1].forEach(function(coin) {
     while (total + coin <= amount) {
       change.push(coin);
@@ -7,4 +7,5 @@ function CoinChanger(amount) {
     }
   });
   return change;
+
 };
