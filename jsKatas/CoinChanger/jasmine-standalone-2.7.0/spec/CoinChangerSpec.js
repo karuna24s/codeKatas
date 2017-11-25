71e0116faeb466a3c1ad751f6e53a99030e8c410
karuna24s <karuna24s@gmail.com>
2017-11-25 14:11:48 -0500

    add third version of coin changers js code kata

--- jsKatas/CoinChanger/jasmine-standalone-2.7.0/spec/CoinChangerSpec.js
+++ jsKatas/CoinChanger/jasmine-standalone-2.7.0/spec/CoinChangerSpec.js
@@ -2,14 +2,14 @@ describe('CoinChanger', function () {
   it ('returns 1 penny for 1', function () {
     expect(CoinChanger(1)).toEqual([1])
   })
-  it ('returns 4 pennies for 4', function () {
-    expect(CoinChanger(4)).toEqual([1, 1, 1, 1])
+  it ('returns 2 pennies for 2', function () {
+    expect(CoinChanger(2)).toEqual([1, 1])
   })
   it ('returns 1 nickel for', function () {
     expect(CoinChanger(5)).toEqual([5])
   })
-  it ('returns 1 nickel, 4 pennies for 9', function () {
-    expect(CoinChanger(9)).toEqual([5, 1, 1, 1, 1])
+  it ('returns 1 nickel, 3 pennies for 8', function () {
+    expect(CoinChanger(8)).toEqual([5, 1, 1, 1])
   })
   it ('returns 1 dime for 1', function () {
     expect(CoinChanger(10)).toEqual([10])
@@ -17,10 +17,13 @@ describe('CoinChanger', function () {
   it ('returns 1 dime, 1 nickel for 15', function () {
     expect(CoinChanger(15)).toEqual([10, 5])
   })
+  it ('returns 1 dime, 1 nickel, 2 pennies for 17', function () {
+    expect(CoinChanger(17)).toEqual([10, 5, 1, 1])
+  })
   it ('returns 1 quarter for 25', function () {
     expect(CoinChanger(25)).toEqual([25])
   })
-  it ('returns 3 quarters, 1 dime, 5, 1 nickel, 4 pennies for 25', function () {
-    expect(CoinChanger(94)).toEqual([25, 25, 25, 10, 5, 1, 1, 1, 1])
+  it ('returns 3 quarters, 1 dime, 5, 1 nickel, 4 pennies for 92', function () {
+    expect(CoinChanger(92)).toEqual([25, 25, 25, 10, 5, 1, 1])
   })
 })
--- jsKatas/CoinChanger/jasmine-standalone-2.7.0/src/CoinChanger.js
+++ jsKatas/CoinChanger/jasmine-standalone-2.7.0/src/CoinChanger.js
@@ -1,11 +1,10 @@
 function CoinChanger(amount) {
   let change = [], total = 0;
-  [25, 10, 5, 1].forEach(function(coin) {
+  [25, 10, 5, 1].map(function(coin) {
     while (total + coin <= amount) {
       change.push(coin);
       total += coin;
     }
   });
   return change;
-
 };
